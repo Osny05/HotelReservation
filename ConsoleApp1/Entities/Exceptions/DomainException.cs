bf988aaa8f4f10d038e62df0b152964ff5446a37
Osny <osnymatheusblins@gmail.com>
2022-02-28 16:19:55 -0300

    first commit

---
+++ ConsoleApp1/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) 
+            : base(message)
+        {
+        }
+    }
+}
